fe852a99e36e45961c67b16931a8125e19b1b0a7
thatielly <202503341656@alunos.ibmec.edu.br>
2025-12-02 11:47:38 -0300

    as

---
+++ as/as.py
@@ -0,0 +1,49 @@
+# AS
+# 1)
+preco = float(input("Digite o preço do produto:"))
+if preco < 50:
+    print("Barato")
+elif preco ( >= 50 and <= 200 ):
+    print("Médio")
+elif preco > 200:
+    print("Caro")
+else:
+    preco = float(input("preço inválido, tente novamente"))
+
+
+# 2) 
+
+quant_cafes_comprados = 8
+peso_por_pacote = 500
+consumo_diario = 35
+consumo_mensal = consumo_diario * 30
+peso_total_disponivel = peso_por_pacote * quant_cafes_comprados - consumo_mensal
+
+print(f"peso total disponivel: {peso_total_disponivel}")
+
+# 3)
+servicos = ["consultoria", "manutenção", "suporte", "treinamento", "limpeza"]
+for s in servicos:
+list_premium = []
+lista_basico = []
+lista_terceirizado = []
+
+    list_premium = ("consultoria, treinamento").list_premium(s)
+    lista_basico = ("manutenção, suporte").lista_basico(s)
+if 
+    lista_terceirizado = 
+
+print(list_premium, lista_basico, lista_terceirizado)
+
+
+
+
+5)
+
+M = 
+i = 
+t = 
+C = 
+
+for lambda 
+valor_final = M = C * t ** i
